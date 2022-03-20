d7a097da1f9d531aff40c35d3deea667b897e95c
Kristiyan Stoyanov <93555141+Storm603@users.noreply.github.com>
2022-03-20 15:34:37 +0200

    Add files via upload

---
+++ 03. Calculations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+
+            if (action == "add")
+            {
+                AddNumbers(numOne, numTwo);
+            }
+            else if (action == "multiply")
+            {
+                MultiplyNumbers(numOne, numTwo);
+            }
+            else if (action == "subtract")
+            {
+                SubtractNumbers(numOne, numTwo);
+            }
+            else if (action == "divide")
+            {
+                DivideNumbers(numOne, numTwo);
+            }
+
+            static void AddNumbers(int numOne, int numTwo)
+            {
+                Console.WriteLine(numOne + numTwo);
+            } 
+            
+            static void MultiplyNumbers(int numOne, int numTwo)
+            {
+                Console.Write(numOne * numTwo);
+            }  
+            
+            static void SubtractNumbers(int numOne, int numTwo)
+            {
+                Console.WriteLine(numOne - numTwo);
+            }  
+            
+            static void DivideNumbers(int numOne, int numTwo)
+            {
+                Console.WriteLine(numOne / numTwo);
+            }
+
+        }
+    }
+}
---
+++ 04. Printing Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int input = int.Parse(Console.ReadLine());
+            PrintTriangle(input);
+        }
+
+        static void PrintTriangle(int input)
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = input - 1; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
